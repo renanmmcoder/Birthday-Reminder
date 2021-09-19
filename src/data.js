375bb59084a1b4827330d574e39ab38ca3dc9d37
Renan <renmms42@gmail.com>
2021-09-19 02:23:58 -0300

    mudancas

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    name: "João",
+  },
+
+  {
+    id: 2,
+    name: "Luiz",
+  },
+
+  {
+    id: 3,
+    name: "Pedro",
+  },
+
+  {
+    id: 4,
+    name: "Ana",
+  },
+
+  {
+    id: 5,
+    name: "João",
+  },
+];
